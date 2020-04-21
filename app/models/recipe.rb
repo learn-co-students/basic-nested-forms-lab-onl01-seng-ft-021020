59819964056a60031ba922822d5e6c575a5f801e
Cathrin Whitmer <catwhitmer@gmail.com>
2020-04-21 13:21:05 +0000

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,9 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients 
+   accepts_nested_attributes_for :ingredients
+   
+   def recipe_attributes=(recipe)
+    self.recipe = Recipe.find_or_create_by(title: recipe[:title])
+    self.recipe.update(recipe)
+  end
 end
